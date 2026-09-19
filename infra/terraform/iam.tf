portable-genai/creative-studio

Make customer-managed encryption optional, and off by default
Every binding to the stack's Cloud KMS key now sits behind cmek_enabled,
default false: the key ring, the key, every grant to a service agent, and
every store, log bucket, registry and revision that took the key. Block
bindings became dynamic blocks; scalar bindings read the key through one(),
which is null when the key does not exist and omits the attribute rather
than pointing it at nothing. The Terraform tests that assert on key material
turn the gate on to keep meaning what they meant.

Why the default is off: a key ring can never be deleted, a log bucket that
has CMEK can never drop it, and registries and document stores take their
key at creation. None of that changes an answer or a screen, and everything
is encrypted at rest with Google-managed keys regardless. A deployment with
a customer whose data it must shred, whose key access must be audited or
whose keys must live in an HSM sets it true in its own tfvars BEFORE the
first apply. Where a stack already applied it, the key's prevent_destroy
refuses a plan that omits the line, which is the right answer: the stores it
bound stay bound.

The vendored deploy-and-residency-hardening skill says the same.

infra/terraform/iam.tf
@@ -45,7 +45,8 @@ resource "google_storage_bucket_iam_member" "runtime_assets" {
 
 # The runtime SA uses the CMEK for envelope ops it performs directly.
 resource "google_kms_crypto_key_iam_member" "runtime" {
-  crypto_key_id = google_kms_crypto_key.creative.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.creative[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:${google_service_account.runtime.email}"
 }

infra/terraform/kms.tf
@@ -10,6 +10,7 @@
 #     global/multi-region key. Regional CMEK pins crypto material in-country.
 
 resource "google_kms_key_ring" "creative" {
+  count    = var.cmek_enabled ? 1 : 0
   name     = "creative-studio-ring"
   location = var.region # the selected region — regional, in-country key material
 
@@ -17,8 +18,9 @@ resource "google_kms_key_ring" "creative" {
 }
 
 resource "google_kms_crypto_key" "creative" {
+  count    = var.cmek_enabled ? 1 : 0
   name     = "creative-studio-cmek"
-  key_ring = google_kms_key_ring.creative.id
+  key_ring = one(google_kms_key_ring.creative[*].id)
 
   purpose         = "ENCRYPT_DECRYPT"
   rotation_period = "7776000s" # 90 days — periodic rotation for key hygiene
@@ -45,28 +47,32 @@ data "google_project" "this" {
 
 # Vertex AI service agent — Imagen image generation, Gemini copy/File Search, Gen AI Evals.
 resource "google_kms_crypto_key_iam_member" "aiplatform" {
-  crypto_key_id = google_kms_crypto_key.creative.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.creative[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:service-${data.google_project.this.number}@gcp-sa-aiplatform.iam.gserviceaccount.com"
 }
 
 # Cloud Storage service agent — CMEK on the generated-creative asset bucket.
 resource "google_kms_crypto_key_iam_member" "storage" {
-  crypto_key_id = google_kms_crypto_key.creative.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.creative[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:service-${data.google_project.this.number}@gs-project-accounts.iam.gserviceaccount.com"
 }
 
 # Cloud Logging service agent — CMEK on the WORM audit bucket.
 resource "google_kms_crypto_key_iam_member" "logging" {
-  crypto_key_id = google_kms_crypto_key.creative.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.creative[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:service-${data.google_project.this.number}@gcp-sa-logging.iam.gserviceaccount.com"
 }
 
 # Cloud Run service agent — CMEK on the service revision.
 resource "google_kms_crypto_key_iam_member" "run" {
-  crypto_key_id = google_kms_crypto_key.creative.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.creative[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:service-${data.google_project.this.number}@serverless-robot-prod.iam.gserviceaccount.com"
 }

infra/terraform/outputs.tf
@@ -35,7 +35,7 @@ output "runtime_service_account" {
 
 output "cmek_key" {
   description = "Regional CMEK crypto key id (protects assets, logs and the Run revision)."
-  value       = google_kms_crypto_key.creative.id
+  value       = one(google_kms_crypto_key.creative[*].id)
 }
 
 output "asset_bucket" {

portable-genai/credit-memo-drafting

infra/terraform/iam.tf
@@ -60,7 +60,8 @@ resource "google_project_iam_member" "app" {
 
 # App uses the CMEK for envelope ops it performs directly.
 resource "google_kms_crypto_key_iam_member" "app" {
-  crypto_key_id = google_kms_crypto_key.credit_memo.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.credit_memo[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:${google_service_account.app.email}"
 }
@@ -98,8 +99,8 @@ resource "google_project_iam_member" "additional_serving" {
 }
 
 resource "google_kms_crypto_key_iam_member" "additional_serving" {
-  for_each      = toset(var.additional_serving_service_accounts)
-  crypto_key_id = google_kms_crypto_key.credit_memo.id
+  for_each      = var.cmek_enabled ? toset(var.additional_serving_service_accounts) : toset([])
+  crypto_key_id = one(google_kms_crypto_key.credit_memo[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:${each.value}"
 }

infra/terraform/kms.tf
@@ -9,6 +9,7 @@
 #         key ring + crypto key here and wire it in per-resource.
 
 resource "google_kms_key_ring" "credit_memo" {
+  count    = var.cmek_enabled ? 1 : 0
   name     = "credit-memo-ring"
   location = var.region # asia-southeast1 — regional, in-country key material (P-05)
 
@@ -16,8 +17,9 @@ resource "google_kms_key_ring" "credit_memo" {
 }
 
 resource "google_kms_crypto_key" "credit_memo" {
+  count    = var.cmek_enabled ? 1 : 0
   name     = "credit-memo-cmek"
-  key_ring = google_kms_key_ring.credit_memo.id
+  key_ring = one(google_kms_key_ring.credit_memo[*].id)
 
   purpose         = "ENCRYPT_DECRYPT"
   rotation_period = "7776000s" # 90 days — periodic rotation for key hygiene
@@ -43,14 +45,16 @@ data "google_project" "this" {
 
 # Vertex AI / Agent Runtime service agent.
 resource "google_kms_crypto_key_iam_member" "aiplatform" {
-  crypto_key_id = google_kms_crypto_key.credit_memo.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.credit_memo[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:service-${data.google_project.this.number}@gcp-sa-aiplatform.iam.gserviceaccount.com"
 }
 
 # Cloud Logging service agent (CMEK on the WORM bucket).
 resource "google_kms_crypto_key_iam_member" "logging" {
-  crypto_key_id = google_kms_crypto_key.credit_memo.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.credit_memo[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:service-${data.google_project.this.number}@gcp-sa-logging.iam.gserviceaccount.com"
 }

infra/terraform/outputs.tf
@@ -16,7 +16,7 @@ output "region" {
 # --------------------------------- KMS -------------------------------------- #
 output "kms_key" {
   description = "Regional CMEK crypto key id (settings.yaml kms_key / CREDIT_MEMO_KMS_KEY)."
-  value       = google_kms_crypto_key.credit_memo.id
+  value       = one(google_kms_crypto_key.credit_memo[*].id)
 }
 
 # ------------------------------- WORM logging ------------------------------- #